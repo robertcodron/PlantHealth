ade7ccd8102a8b11557552afe72b7916d2c4a930
Robert CODRON <robert.codron@gmail.com>
2021-06-10 15:17:32 +0200

    adding a skeleton for flask

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+load_dotenv()
+
 
 app = Flask(__name__)
+#env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(Config)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+from . import routes, models
 
 
 if __name__ == '__main__':
---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv()
+
+class Config:
+    DEBUG = False
+    DEVELOPMENT = False
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    CSRF_ENABLED = True
+    #SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    pass
+
+class StagingConfig(Config):
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    DEVELOPMENT = True
